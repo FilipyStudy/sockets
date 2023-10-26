7cb74605144e10eb9b73faa65abca8fec9d4698a
Filipy <89490624+FilipyStudy@users.noreply.github.com>
2023-10-26 16:24:38 -0300

    Update server.py
    New Functions added for future features.

--- server.py
+++ server.py
@@ -1,6 +1,32 @@
 import socket
 import threading
+import mysql.connector
+from hashlib import sha256
 
+def login_try():
+    username = input("Enter the username: ")
+    password = input("Enter the password: ")
+    hash_pass = sha256((username + password).encode("utf-8")).hexdigest()
+    hash_check = sha256((hash_pass + username).encode("utf-8")).hexdigest()
+    return hash_check
+connector = mysql.connector.connect(
+    host="localhost",
+    password="password",
+    user="root"
+)
+cursor = connector.cursor()
+
+login = 'admin'
+psw = 'password_hash'
+hash_check = 'hash_check (password_hash + username)'
+def login_verify():
+    try:
+        cursor.execute(f"SELECT * FROM python_sockets.login WHERE username = '{login}'"
+                       f" AND psw = '{psw}'"
+                       f" AND hash_check = '{hash_check}'")
+        print("Sucesfull")
+    except Exception as e:
+        print(f"Erro: {e}")
 
 def run_server():
     server_ip = "192.168.100.19"
@@ -20,8 +46,6 @@ def run_server():
         print(f"Error occurs: {e}")
     finally:
         server.close()
-
-
 def handle_client(client_sock, client_addr):
     try:
         print(f"Accepted connection with: {client_addr[0]}:{client_addr[1]}")
@@ -40,5 +64,3 @@ def handle_client(client_sock, client_addr):
         client_sock.close()
         print(f"Connection to client {client_addr[0]}:{client_addr[1]} closed.")
 
-
-run_server()
