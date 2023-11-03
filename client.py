5fb3cabe80e28677d18b47350543963cc04968e0
Filipy <89490624+FilipyStudy@users.noreply.github.com>
2023-11-03 15:35:35 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,17 +1,24 @@
 import socket
-
-
+from hashlib import sha256
+import pickle
 def run_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_ip = "192.168.100.19"
+    server_ip = "192.168.100.103"
     server_port = 5555
-
+    connection = True
     client.connect((server_ip, server_port))
+    message = login_try()
+    client.send(message)
 
-    while True:
-        message = input("Enter your message: ")
+    log_auth = client.recv(1024)
+    log_auth = log_auth.decode("utf-8")
+
+    if log_auth != "True": connection == False
+
+    while connection:
+
+        message = input("Type your message: ")
         client.send(message.encode("utf-8"))
-
         response = client.recv(1024)
         response = response.decode("utf-8")
 
@@ -24,5 +31,11 @@ def run_client():
     client.close()
     print("Connection to server as been closed.")
 
+def login_try():
+    username = input("Enter the username: ")
+    password = input("Enter the password: ")
+    hash_pass = sha256((username + password).encode("utf-8")).hexdigest()
+    hash_check = sha256((hash_pass + username).encode("utf-8")).hexdigest()
+    return pickle.dumps([username, hash_check])
 
 run_client()
